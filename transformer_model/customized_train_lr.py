748b607f0ac865aadab4898c90bf2687bc168034
Darren <38484622+lwind18@users.noreply.github.com>
2024-04-04 16:37:24 -0500

    Update customized_train_lr.py

--- transformer_model/customized_train_lr.py
+++ transformer_model/customized_train_lr.py
@@ -1,7 +1,6 @@
 ## import customized_train
 import math
 import logging
-
 import numpy as np 
 import tensorflow as tf 
 # import tensorflow-addons as tfa 
@@ -37,7 +36,6 @@ class LRTensorBoard(TensorBoard):
         
         logs.update({'lr': K.eval(current_lr)})
         super().on_epoch_end(epoch, logs)
-
  
 def my_train_schedule(model,train_datasetx,train_datasety,epochs=10,start_rate=0.001,loss=np.nan,per_epoch=100,split_epoch=4,option=0):
     momentum = 0.9   # good for RMSprop not good for RMSprop but OK for Adam
@@ -78,66 +76,6 @@ def my_train_schedule(model,train_datasetx,train_datasety,epochs=10,start_rate=0
     model.compile(optimizer=optimizer, loss=loss, metrics=['accuracy',lr_metric])
     model_history = model.fit(x=train_datasetx, y=train_datasety, validation_split=0.04, epochs=epochs-split_epoch, verbose=2)
     return model_history
-
-## training with validation data using RMSprop
-def my_train_RMSprop(model,train_dataset, validation_dataset, epochs=2, BATCH_SIZE=4,start_rate=0.001,loss=np.nan):
-    whichMAX = 2
-    each_EPOCH = 30
-    EPOCHS_ALL = [50,30,20]
-    # if BATCH_SIZE<20:
-        # EPOCHS_ALL = [30,40,30]
-    
-    # EPOCHS_ALL = [20,20,20]
-    
-    # EPOCHS_ALL = [50,50,50]
-    # whichMAX = 1
-    # each_EPOCH = 50
-    momentum = 0.9   # good for RMSprop not good for RMSprop but OK for Adam
-    print_str = "momentum="+str(momentum)
-    print(print_str)
-    logging.info(print_str)
-    whichi = 0
-    # for traini in range(0,epochs,each_EPOCH):
-    for traini in range(whichMAX+1):
-        rate = start_rate*(5**(whichi*-1) )
-        # epoch_n_i = min(epochs-traini,each_EPOCH)
-        epoch_n_i = EPOCHS_ALL[traini]
-        print_str = "learning_rate="+str(rate)
-        print(print_str)
-        logging.info(print_str)
-        # loss = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True)
-        model.compile(optimizer=tf.keras.optimizers.RMSprop(learning_rate=rate, rho=momentum), loss=loss, metrics=['accuracy'])
-        print ('tf.keras.optimizers.RMSprop')
-        # model.compile(optimizer=tf.keras.optimizers.SGD(learning_rate=rate, momentum=momentum), loss=loss, metrics=['accuracy'])
-        model_history = model.fit(train_dataset, validation_data=validation_dataset, epochs=epoch_n_i,verbose=2)
-        whichi = whichi+1 if whichi<whichMAX else whichi
-
-## training with validation data using Adam
-def my_train_Adam(model,train_dataset, validation_dataset, epochs=2, BATCH_SIZE=4,start_rate=0.001):
-# def my_train2(model,train_dataset, validation_dataset, epochs=2, BATCH_SIZE=4,start_rate=0.0001):
-    whichMAX = 2
-    whichMAX = 1
-    each_EPOCH = 20
-    momentum = 0.9   # not good for RMSprop but OK for Adam
-    # momentum = 0.99  # good 
-    # momentum = 0.999 # not good v5.1
-    # momentum = 0.995 # had no difference with 0.99 so 0.99 is used 
-    print_str = "momentum="+str(momentum)
-    print(print_str)
-    logging.info(print_str)
-    whichi = 0
-    for traini in range(0,epochs,each_EPOCH):
-        rate = start_rate*10**(whichi*-1)
-        print_str = "learning_rate="+str(rate)
-        print(print_str)
-        logging.info(print_str)
-        loss = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True)
-        model.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=rate, beta_1=momentum), loss=loss, metrics=['accuracy'])
-        print ('tf.keras.optimizers.Adam')
-        # model.compile(optimizer=tf.keras.optimizers.RMSprop(learning_rate=rate, rho=momentum), loss=loss, metrics=['accuracy'])
-        # model.compile(optimizer=tf.keras.optimizers.SGD(learning_rate=rate, momentum=momentum), loss=loss, metrics=['accuracy'])
-        model_history = model.fit(train_dataset, validation_data=validation_dataset, epochs=each_EPOCH,verbose=2)
-        whichi = whichi+1 if whichi<whichMAX else whichi
 
 def my_loss(y_pred,y_true):
     one_hot_y = tf.one_hot(y_true,N_CLASS)
